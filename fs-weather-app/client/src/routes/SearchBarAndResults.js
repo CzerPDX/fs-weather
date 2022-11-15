2107e449e8f546dbcc372ea8bed713a0c279a25d
CzerPDX <czer@pdx.edu>
2022-11-15 15:27:53 -0800

    small changes

--- fs-weather-app/client/src/routes/SearchBarAndResults.js
+++ fs-weather-app/client/src/routes/SearchBarAndResults.js
@@ -1,13 +1,13 @@
 // MainSearch manages the search function on the main page
 
 import React from 'react'
-import LargeSearchBar from '../components/general/search-bars/LargeSearchBar'
+import MainSearch from '../components/general/MainSearch'
 import ForecastCardSimple from '../components/forecast-cards/ForecastCardSimple'
 
 const SearchBarAndResults = () => {
   return (
     <div className='main-search container'>
-      <LargeSearchBar 
+      <MainSearch 
         defaultText='Search by city and state...'
       />
       <ForecastCardSimple />
