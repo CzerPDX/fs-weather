c78c1bbae4c32c8c55f97f3c3cf0af79a13ef315
ELondonJ <ljoseph@pdx.edu>
2022-11-15 17:19:46 -0800

    removed logic from return statement

--- fs-weather-app/src/components/general/search-bars/SearchAutoComplete.js
+++ fs-weather-app/src/components/general/search-bars/SearchAutoComplete.js
@@ -6,6 +6,35 @@ import PlacesAutocomplete, {
 
 const SearchAutoComplete = ({ setCoordinates }) => {
   const [city, setCity] = useState("");
+  const renderFunc = ({
+    getInputProps,
+    suggestions,
+    getSuggestionItemProps,
+    loading,
+  }) => (
+    <div>
+      <input
+        className="form-control form-control-lg"
+        {...getInputProps({ placeholder: "City" })}
+      />
+
+      <div>
+        {loading ? <div>...</div> : ""}
+        {suggestions.map((suggestion) => {
+          const style = {
+            backgroundColor: suggestion.active && "green",
+            color: suggestion.active && "white",
+          };
+          console.log(suggestion);
+          return (
+            <div {...getSuggestionItemProps(suggestion, { style })}>
+              {suggestion.description}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 
   // Called once a user selects from the list of suggested cities. Populates
   // useState variable coordinates with the selected cities lat and lng
@@ -24,30 +53,7 @@ const SearchAutoComplete = ({ setCoordinates }) => {
         onChange={setCity}
         onSelect={handleSelect}
       >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div>
-            <input
-              className="form-control form-control-lg"
-              {...getInputProps({ placeholder: "City" })}
-            />
-
-            <div>
-              {loading ? <div>...</div> : ""}
-              {suggestions.map((suggestion) => {
-                const style = {
-                  backgroundColor: suggestion.active && "green",
-                  color: suggestion.active && "white",
-                };
-                console.log(suggestion);
-                return (
-                  <div {...getSuggestionItemProps(suggestion, { style })}>
-                    {suggestion.description}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
+        {renderFunc}
       </PlacesAutocomplete>
     </div>
   );
