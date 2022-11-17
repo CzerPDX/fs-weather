0a7b8dfbc00aeae51fc9085f5ad5693f9c0325eb
ELondonJ <ljoseph@pdx.edu>
2022-11-17 10:36:36 -0800

    backend calls to openweather api for air quality,  current, hourly and 7 day weather forecasts implemented

--- fs-weather-app/server/fs-server.js
+++ fs-weather-app/server/fs-server.js
@@ -1,30 +1,94 @@
-const express = require('express')
-const app = express()
-const port = 5001
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 5001;
+const KEY = "1e859cb9e9c8898c11b003c46405b5d2";
 
 // Set up a route for the API. The frontend will make calls to this route
 // In the frontend we will fetch this user array and display all the users.
 app.get("/api*", (req, res) => {
-  
   // Handle requests for /api-current-weather
-  if (req.url === '/api-current-weather') {
-    // Manually saving a response obj. We will replace this with a call to the API later.
-    // let responseObject = '[{"coord":{"lon":-122.6784,"lat":45.5152},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":39.99,"feels_like":35.74,"temp_min":35.35,"temp_max":44.17,"pressure":1027,"humidity":86},"visibility":10000,"wind":{"speed":5.99,"deg":53,"gust":14},"clouds":{"all":0},"dt":1668393336,"sys":{"type":2,"id":2034915,"country":"US","sunrise":1668352038,"sunset":1668386576},"timezone":-28800,"id":5746545,"name":"Portland","cod":200}]'
-    let responseObject = '{"coord":{"lon":-122.6784,"lat":45.5152},"weather":[{"id":622,"main":"Snow","description":"Heavy shower snow","icon":"13d"}],"base":"stations","main":{"temp":39.99,"feels_like":35.74,"temp_min":35.35,"temp_max":44.17,"pressure":1027,"humidity":86},"visibility":10000,"wind":{"speed":5.99,"deg":53,"gust":14},"clouds":{"all":0},"dt":1668393336,"sys":{"type":2,"id":2034915,"country":"US","sunrise":1668352038,"sunset":1668386576},"timezone":-28800,"id":5746545,"name":"Portland","cod":200}'
-    
+  if (req.url.split("?")[0] === "/api-current-weather") {
+    //
+    let lat = req.query.lat;
+    let lng = req.query.lng;
+    let responseObject = null;
+    console.log("***current", req.query);
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=imperial&appid=${KEY}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        res.status(200).send(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     // We will let the front-end parse the json data since it seems like that's how the openweather api is
-    res.send(responseObject)
+  } else if (req.url.split("?")[0] === "/api-hourly-weather") {
+    //
+    let lat = req.query.lat;
+    let lng = req.query.lng;
+    let responseObject = null;
+    console.log("***hourly");
+    axios
+      .get(
+        `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${lng}&appid=${KEY}&cnt=12`
+      )
+      .then((response) => {
+        console.log(response.data);
+        //res.status(200).send(response.data);
+        res.end;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else if (req.url.split("?")[0] === "/api-forecast-weather") {
+    let lat = req.query.lat;
+    let lng = req.query.lng;
+    let responseObject = null;
+    console.log("***forecast");
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lng}&appid=${KEY}&cnt=7`
+      )
+      .then((response) => {
+        console.log(response.data);
+        //res.status(200).send(response.data);
+        res.end;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // We will let the front-end parse the json data since it seems like that's how the openweather api is
+  } else if (req.url.split("?")[0] === "/api-air-quality") {
+    let lat = req.query.lat;
+    let lng = req.query.lng;
+    let responseObject = null;
+    console.log("***air quality");
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lng}&appid=${KEY}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        //res.status(200).send(response.data);
+        res.end;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // We will let the front-end parse the json data since it seems like that's how the openweather api is
   }
 
   // Handle invalid requests
   else {
-    console.error('Error! Response was empty')
-    res.send('[]')
+    console.error("Error! Response was empty");
+    res.send("[]");
   }
-})
+});
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}...`)
-})
-
-
+  console.log(`Server started on port ${port}...`);
+});
