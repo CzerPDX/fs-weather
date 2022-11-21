7693dc6f604a1fcbfdca4a3144434799138bce3e
ELondonJ <ljoseph@pdx.edu>
2022-11-20 22:13:21 -0800

    user input location connected to forecast card values

--- fs-weather-app/client/src/components/forecasts/CurrentWeather.js
+++ fs-weather-app/client/src/components/forecasts/CurrentWeather.js
@@ -10,38 +10,34 @@
     https://bobbyhadz.com/blog/react-hook-useeffect-has-missing-dependency
 */
 
-import PropTypes from 'prop-types'
-import React, { useEffect, useState, useCallback } from 'react'
+import PropTypes from 'prop-types';
+import React, { useEffect, useState, useCallback } from 'react';
 import ForecastCard from './ForecastCard';
 // import React, { useState } from 'react'
 
-
-const CurrentWeather = ( props ) => {
-
-
+const CurrentWeather = (props) => {
   // Have to set the data up with defaults in the same structure they will be in
   // when returned or the software will freak out
   const initialWeatherState = {
-    'locationName': 'Loading...',
-    'temperature': 'Loading...',
-    'shortDescription': 'Loading...',
-    'longDescription': 'Loading...',
-    'iconCode': 'Loading...'
-  }
-
+    locationName: 'Loading...',
+    temperature: 'Loading...',
+    shortDescription: 'Loading...',
+    longDescription: 'Loading...',
+    iconCode: 'Loading...',
+  };
 
   const url = `/api-current-weather?lat=${props.lat}&lon=${props.lon}`;
   // Get data from backend API
-  const [weatherData, setWeatherData] = useState(initialWeatherState)
+  const [weatherData, setWeatherData] = useState(initialWeatherState);
 
   // Declare the async data fetching function
   const fetchData = useCallback(async () => {
-    const response = await fetch(url)
+    const response = await fetch(url);
     // Convert the data to json
     const json = await response.json();
     console.log(`SETTING WEATHER DATA: ${JSON.stringify(json)}`);
     setWeatherData(json);
-  }, [url])
+  }, [url]);
 
   // the useEffect is only there to call `fetchData` at the right time
   // This allow us to use the callback to send the data back out of the useEffect
@@ -49,43 +45,40 @@ const CurrentWeather = ( props ) => {
   useEffect(() => {
     fetchData()
       // Error handling
-      .catch(console.error)
-  }, [fetchData])
-
+      .catch(console.error);
+  }, [props]);
 
   return (
-    <div className='current-weather-detailed'>
+    <div className="current-weather-detailed">
       {/* Forecast Title will stay on this component*/}
-      <div className='title-block'>
+      <div className="title-block">
         <h5>Showing the weather from {weatherData.locationName}:</h5>
       </div>
 
-      <div className='forecast-container'>
+      <div className="forecast-container">
         <ForecastCard
           cardType={props.cardType}
-          iconCode={weatherData.iconCode} 
-          shortDescription={weatherData.shortDescription} 
-          longDescription={weatherData.longDescription} 
+          iconCode={weatherData.iconCode}
+          shortDescription={weatherData.shortDescription}
+          longDescription={weatherData.longDescription}
           temperature={weatherData.temperature}
         />
         <ForecastCard
           cardType={props.cardType}
-          iconCode={weatherData.iconCode} 
-          shortDescription={weatherData.shortDescription} 
-          longDescription={weatherData.longDescription} 
+          iconCode={weatherData.iconCode}
+          shortDescription={weatherData.shortDescription}
+          longDescription={weatherData.longDescription}
           temperature={weatherData.temperature}
         />
       </div>
-      
     </div>
-  )
-}
-
+  );
+};
 
 CurrentWeather.propTypes = {
   lat: PropTypes.string.isRequired,
   lon: PropTypes.string.isRequired,
   cardType: PropTypes.string.isRequired,
-}
+};
 
-export default CurrentWeather 
+export default CurrentWeather;
--- fs-weather-app/client/src/components/forecasts/SearchBarAndResults.js
+++ fs-weather-app/client/src/components/forecasts/SearchBarAndResults.js
@@ -1,30 +1,38 @@
 // MainSearch manages the search function on the main page
 
-import React from 'react'
-import CurrentWeather from './CurrentWeather'
-import MainSearch from '../general/search-bars/MainSearch'
+import { useState } from 'react';
+import React from 'react';
+import CurrentWeather from './CurrentWeather';
+import MainSearch from '../general/search-bars/MainSearch';
 
 const SearchBarAndResults = () => {
-  let lat = '45.5152'
-  let lon = '-122.6784'
-
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lgn: null,
+  });
+  let show;
+  if (coordinates.lat === null && coordinates.lgn === null) {
+    show = '';
+  } else {
+    show = (
+      <CurrentWeather
+        lat={coordinates.lat}
+        lon={coordinates.lon}
+        cardType="main-large"
+      />
+    );
+  }
   // const latAndlon = (latFromSearch, lonFromSearch) => {
   //   lat = latFromSearch
   //   lon = lonFromSearch
   // }
 
   return (
-    <div className='main-page container'>
-      <MainSearch />
-
-      <CurrentWeather 
-        lat={lat}
-        lon={lon}
-        cardType='main-large'
-      />
-
+    <div className="main-page container">
+      <MainSearch setCoordinates={setCoordinates} coordinates={coordinates} />
+      {show}
     </div>
-  )
-}
+  );
+};
 
-export default SearchBarAndResults
+export default SearchBarAndResults;
