2ced62b7c42ee473bbc6fb1931090ea3e4c89fe0
ELondonJ <ljoseph@pdx.edu>
2022-11-25 13:35:19 -0800

    moved coordinate state declaration to App.js. Restructuring for routing between different forecasts

--- fs-weather-app/client/src/App.js
+++ fs-weather-app/client/src/App.js
@@ -1,12 +1,43 @@
 import './index.css';
 import MainPage from './routes/MainPage';
+import { useState } from 'react';
+import TestCurrentWeather from './components/forecasts/TestCurrentWeather';
+import Header from './components/general/Header';
+import SearchBarAndResults from './components/forecasts/SearchBarAndResults';
 
 const App = () => {
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lgn: null,
+  });
+  let show;
+  if (coordinates.lat === null && coordinates.lgn === null) {
+    show = '';
+  } else {
+    show = (
+      <div>
+        <TestCurrentWeather
+          lat={coordinates.lat}
+          lon={coordinates.lon}
+          cardType="main-large"
+        />
+        <TestCurrentWeather
+          lat={coordinates.lat}
+          lon={coordinates.lon}
+          cardType="hourly-forecast"
+        />
+      </div>
+    );
+  }
   return (
-    <div>
-      <MainPage />
+    <div className="mainPage">
+      <Header headerTitle="FS Weather" headerClasses="p-3 bg-dark text-white" />
+      <SearchBarAndResults
+        setCoordinates={setCoordinates}
+        coordinates={coordinates}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- fs-weather-app/client/src/components/forecasts/SearchBarAndResults.js
+++ fs-weather-app/client/src/components/forecasts/SearchBarAndResults.js
@@ -6,30 +6,30 @@ import CurrentWeather from './CurrentWeather';
 import TestCurrentWeather from './TestCurrentWeather';
 import MainSearch from '../general/search-bars/MainSearch';
 
-const SearchBarAndResults = () => {
-  const [coordinates, setCoordinates] = useState({
-    lat: null,
-    lgn: null,
-  });
-  let show;
-  if (coordinates.lat === null && coordinates.lgn === null) {
-    show = '';
-  } else {
-    show = (
-      <div>
-        <TestCurrentWeather
-          lat={coordinates.lat}
-          lon={coordinates.lon}
-          cardType="main-large"
-        />
-        <TestCurrentWeather
-          lat={coordinates.lat}
-          lon={coordinates.lon}
-          cardType="hourly-forecast"
-        />
-      </div>
-    );
-  }
+const SearchBarAndResults = ({ coordinates, setCoordinates }) => {
+  // const [coordinates, setCoordinates] = useState({
+  //   lat: null,
+  //   lgn: null,
+  // });
+  // let show;
+  // if (coordinates.lat === null && coordinates.lgn === null) {
+  //   show = '';
+  // } else {
+  //   show = (
+  //     <div>
+  //       <TestCurrentWeather
+  //         lat={coordinates.lat}
+  //         lon={coordinates.lon}
+  //         cardType="main-large"
+  //       />
+  //       <TestCurrentWeather
+  //         lat={coordinates.lat}
+  //         lon={coordinates.lon}
+  //         cardType="hourly-forecast"
+  //       />
+  //     </div>
+  //   );
+  // }
   // const latAndlon = (latFromSearch, lonFromSearch) => {
   //   lat = latFromSearch
   //   lon = lonFromSearch
