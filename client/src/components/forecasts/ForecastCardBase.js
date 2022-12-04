18528dc400b5f779047d16f5929c96e9b1d23b07
CzerPDX <czer@pdx.edu>
2022-12-03 17:35:36 -0800

    removed MainLarge as it was not being used

--- client/src/components/forecasts/ForecastCardBase.js
+++ client/src/components/forecasts/ForecastCardBase.js
@@ -4,53 +4,6 @@ import HourlyCard from './forecast-display-cards/HourlyCard';
 import DailyCard from './forecast-display-cards/DailyCard';
 import CurrentCard from './forecast-display-cards/CurrentCard';
 
-const WeatherImage = ({ icon, shortDescription }) => {
-  return (
-    <video autoPlay loop muted className="video">
-      <source src={backgroundVideo} type="video/mp4" />
-    </video>
-  );
-};
-
-// Different cardTypes of ForecastCards can have different layouts
-// Add new layouts as new components below like MainLarge is
-
-const MainLarge = ({
-  temperature,
-  iconCode,
-  shortDescription,
-  longDescription,
-}) => {
-  return (
-    <div>
-      <div className="temperature-and-graphic-card">
-        <div className="temperature row">
-          <div className="col">
-            <h1>{temperature}</h1>
-          </div>
-        </div>
-        {/* This image will also be a component eventually so it doesn't start as a broken link before load */}
-        {/* It will also eventually change depending on the icon code because it tracks weather conditions and time of day */}
-        <div className="weather-icon row">
-          <div className="col">
-            <WeatherImage
-              iconCode={iconCode}
-              shortDescription={shortDescription}
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="detailed-weather-info">
-        <div className="short-description">Description: {shortDescription}</div>
-        <div className="long-description">
-          Long description: {longDescription}
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const ForecastCardBase = (props) => {
   // Pick layout of forecast card based on the card type
   const renderByType = (props) => {
@@ -80,13 +33,6 @@ const ForecastCardBase = (props) => {
 
 ForecastCardBase.propTypes = {
   cardType: PropTypes.string.isRequired,
-};
-
-MainLarge.propTypes = {
-  iconCode: PropTypes.string.isRequired,
-  shortDescription: PropTypes.string.isRequired,
-  longDescription: PropTypes.string.isRequired,
-  temperature: PropTypes.string.isRequired,
 };
 
 export default ForecastCardBase;
